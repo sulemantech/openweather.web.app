b64944eddf4181465c01c3712d7dfa551e828c01
Sulman <shahsuliman@gmail.com>
2022-04-27 03:23:34 +0500

    added code first and migration to the project

---
+++ openweather.web.app/Models/OpenWeatherContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace openweather.web.app.Models
+{
+    public class OpenWeatherContext : DbContext
+
+    {
+        protected OpenWeatherContext()
+        {
+        }
+        public DbSet<Weather> Weathers { get; set; }
+        public DbSet<WeatherForecast> Forecasts { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ openweather.web.app/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace openweather.web.app.Models
+{
+    public class Product
+    {
+       
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal ? Price { get; set; }
+
+
+    }
+}
